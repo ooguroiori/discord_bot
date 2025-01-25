eec029c39fa42c1da9dd93651885115fea9e70b1
ooguroiori <iori.oguro@g.dreamcareer.co.jp>
2025-01-25 15:21:38 +0900

    feat: マイクラサーバー管理bot

---
+++ micrabot.py
@@ -0,0 +1,44 @@
+import os
+import discord
+from discord.ext import commands
+import boto3
+from dotenv import load_dotenv
+
+# .envファイルを読み込む
+load_dotenv()
+
+# 環境変数を取得
+DISCORD_TOKEN = os.getenv('DISCORD_MINECRAFT_TOKEN')
+AWS_REGION = os.getenv('AWS_REGION')
+INSTANCE_ID = os.getenv('AWS_INSTANCE_ID')
+
+# AWSクライアント
+ec2 = boto3.client('ec2', region_name=AWS_REGION)
+
+# Discordボット設定
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'ログインしました: {bot.user}')
+
+@bot.command()
+async def start_ec2(ctx):
+    """EC2インスタンスを起動するコマンド"""
+    try:
+        ec2.start_instances(InstanceIds=[INSTANCE_ID])
+        await ctx.send(f'インスタンス {INSTANCE_ID} を起動しています...')
+    except Exception as e:
+        await ctx.send(f'エラー: {e}')
+
+@bot.command()
+async def stop_ec2(ctx):
+    """EC2インスタンスを停止するコマンド"""
+    try:
+        ec2.stop_instances(InstanceIds=[INSTANCE_ID])
+        await ctx.send(f'インスタンス {INSTANCE_ID} を停止しています...')
+    except Exception as e:
+        await ctx.send(f'エラー: {e}')
+
+bot.run(DISCORD_TOKEN)
